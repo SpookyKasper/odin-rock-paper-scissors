a7ed3b75069552dd941b910e0427e441755ca35a
Kasper <kasperbak89@pm.me>
2022-04-14 03:21:00 +0200

    Create function randomly gets integer 0, 1, or 2

--- rock-paper-script.js
+++ rock-paper-script.js
@@ -3,17 +3,10 @@ console.log("Hello World");
 
 /* Create a rock paper scissors game where the player plays against the computer */
 
-/* Create a function named 'computerPlay' that randomly returns either 'Rock', 'Paper' or 'Scissors' */
-
 /* Create a function that randomly gets a integer between 0 and passed argument */
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max)
-};
-
-console.log(getRandomInt(3));
-
-function computerPlay() {
 };
 
 /* Create an array object containing computer options for 'computerPlay' function */
@@ -22,6 +15,17 @@ let rockPaSc = new Array('Rock', 'Paper', 'Scissors');
 
 console.log(rockPaSc);
 console.log(typeof(rockPaSc));
+
+/* Create a function named 'getUpTo3' thant gets a number between 0 and 3 */
+
+function getUpTo3() {
+    return getRandomInt(3);
+};
+
+/* Create a function named 'computerPlay' that randomly returns either 'Rock', 'Paper' or 'Scissors' */
+
+console.log(getUpTo3());
+
 
 
 
