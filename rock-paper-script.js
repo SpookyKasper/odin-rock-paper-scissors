b79b14a56fee8f0b16e25e0df8daa4b94ea9b947
Kasper <kasperbak89@pm.me>
2022-04-14 04:11:39 +0200

    Update comments and clear undesired function

--- rock-paper-script.js
+++ rock-paper-script.js
@@ -1,37 +1,29 @@
 console.log("Hello World");
 
 
-/* Create a Rock Paper Scissors game where the player plays against the computer */
+/*---------------------------------- Create a Rock Paper Scissors game where the player plays against the computer --------------------------------*/
 
-/* Create a function that randomly 
-gets a integer between 0 and passed argument */
+/* Create a function named 'getRandomInt' that 
+randomly returns an integer between 0 and passed argument */
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max)
 };
 
-/* Create an array object containing 
-computer options for the game */
+/* Create an array object named 'rockPaSc' that
+contains the 3 computer options for the game */
 
 let rockPaSc = new Array('Rock', 'Paper', 'Scissors');
 
 console.log(rockPaSc);
 console.log(typeof(rockPaSc));
 
-/* Create a function named 'getUpTo3' that gets 
-a number between 0 and 3 */
-
-function getUpTo3() {
-    return getRandomInt(3);
-};
-
-console.log(getUpTo3());
-
-/* Create a function named 'computerPlay' that randomly 
-returns either 'Rock', 'Paper' or 'Scissors' */
+/* Create a function named 'computerPlay' that 
+uses 'getUpto3' and 
+randomly returns either 'Rock', 'Paper' or 'Scissors' */
 
 function computerPlay() {
-    return rockPaSc[getUpTo3()];
+    return rockPaSc[getRandomInt(3)];
 }
 
 console.log(computerPlay());
@@ -41,6 +33,7 @@ console.log(typeof(computerPlay()));
 Rock Paper Scissors, it takes two Parameters '
 playerSelection' and 'computerSelection'
 and then return a string that declares the winner */
+
 
 let winString = "You Lose! Paper beats Rock";
 
