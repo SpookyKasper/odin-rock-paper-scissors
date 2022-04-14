f95aa82541a4fc1dd9d15113df3204bee9ef574d
Kasper <kasperbak89@pm.me>
2022-04-14 02:38:05 +0200

    Update algorithm, create function 'computerPlay'

--- rock-paper-script.js
+++ rock-paper-script.js
@@ -3,12 +3,12 @@ console.log("Hello World");
 
 /* Create a rock paper scissors game where the player plays against the computer */
 
-/* Create an array containing computer options for 'computerPlay' function
-
 /* Create a function named 'computerPlay' that randomly returns either 'Rock', 'Paper' or 'Scissors' */
 
-
-
-let computerPlay = function() {
-    
+function computerPlay() {
 }
+
+/* Create an array containing computer options for 'computerPlay' function */
+
+
+
