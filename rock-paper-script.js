21e0c57720c75a612c3896071636d01aca307d29
Kasper <kasperbak89@pm.me>
2022-04-14 02:22:13 +0200

    Add pseudocode comments to js (building algorithm)

--- rock-paper-script.js
+++ rock-paper-script.js
@@ -1,1 +1,14 @@
 console.log("Hello World");
+
+
+/* Create a rock paper scissors game where the player plays against the computer */
+
+/* Create an array containing computer options for 'computerPlay' function
+
+/* Create a function named 'computerPlay' that randomly returns either 'Rock', 'Paper' or 'Scissors' */
+
+
+
+let computerPlay = function() {
+    
+}
