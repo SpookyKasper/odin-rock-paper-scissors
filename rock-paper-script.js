670a2b489a206b944c0b557cb2e7fb3a784d61ba
Kasper <kasperbak89@pm.me>
2022-04-14 03:52:46 +0200

    Declares variable to stock winning string

--- rock-paper-script.js
+++ rock-paper-script.js
@@ -1,22 +1,25 @@
 console.log("Hello World");
 
 
-/* Create a rock paper scissors game where the player plays against the computer */
+/* Create a Rock Paper Scissors game where the player plays against the computer */
 
-/* Create a function that randomly gets a integer between 0 and passed argument */
+/* Create a function that randomly 
+gets a integer between 0 and passed argument */
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max)
 };
 
-/* Create an array object containing computer options for 'computerPlay' function */
+/* Create an array object containing 
+computer options for the game */
 
 let rockPaSc = new Array('Rock', 'Paper', 'Scissors');
 
 console.log(rockPaSc);
 console.log(typeof(rockPaSc));
 
-/* Create a function named 'getUpTo3' that gets a number between 0 and 3 */
+/* Create a function named 'getUpTo3' that gets 
+a number between 0 and 3 */
 
 function getUpTo3() {
     return getRandomInt(3);
@@ -24,7 +27,8 @@ function getUpTo3() {
 
 console.log(getUpTo3());
 
-/* Create a function named 'computerPlay' that randomly returns either 'Rock', 'Paper' or 'Scissors' */
+/* Create a function named 'computerPlay' that randomly 
+returns either 'Rock', 'Paper' or 'Scissors' */
 
 function computerPlay() {
     return rockPaSc[getUpTo3()];
@@ -32,6 +36,17 @@ function computerPlay() {
 
 console.log(computerPlay());
 console.log(typeof(computerPlay()));
+
+/* Write a function that plays a single round of 
+Rock Paper Scissors, it takes two Parameters '
+playerSelection' and 'computerSelection'
+and then return a string that declares the winner */
+
+let winstring = "You Lose! Paper beats Rock";
+
+function oneRound(playerSelection, computerSelection){
+    
+}
 
 
 
