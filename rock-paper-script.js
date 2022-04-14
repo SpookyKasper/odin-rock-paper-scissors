f8284884ad37210726ee23e1c4085d6c2c4b6fde
Kasper <kasperbak89@pm.me>
2022-04-14 03:01:28 +0200

    Add function to get Integer between 0 and 3

--- rock-paper-script.js
+++ rock-paper-script.js
@@ -5,8 +5,16 @@ console.log("Hello World");
 
 /* Create a function named 'computerPlay' that randomly returns either 'Rock', 'Paper' or 'Scissors' */
 
+/* Create a function that randomly gets a integer between 0 and passed argument */
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max)
+};
+
+console.log(getRandomInt(3));
+
 function computerPlay() {
-}
+};
 
 /* Create an array object containing computer options for 'computerPlay' function */
 
