efc71208cf3470ad448600d1690a7ab649501b5e
Kasper <kasperbak89@pm.me>
2022-04-18 19:08:56 +0200

    Add score tracking

--- rock-paper-script.js
+++ rock-paper-script.js
@@ -32,25 +32,30 @@ return a string that declares the winner */
 /* One Round:
 Ask user to pick Rock Paper or Scissors and store user input in variable 'playerSelection
 Generate computer Pick and store it in a variable 'playerSelection'
-Compare the two picks and returns according */
+Compare the two picks and return desired phrase according to the result of the comparaison */
 
 function oneRound(playerSelection, computerSelection) {
+
     /* Declares variable named 'playerChoice' of type string that
     stores capitalized user input of the prompt */
     let playerChoice = capitalizeStrings(prompt('Rock Paper or Scissors ?'));
+
+    /* Assign playerChoice value to playerSelection and logs it */
     playerSelection = playerChoice;
     console.log(`Your selection was ${playerSelection}`);
 
     /* Declares variable named 'playerChoice' of type string that
     stores computer result of computerPlay function */
     const computerChoice = computerPlay();
+
+    /* Assigns computerChoice value to computerSelection and logs it*/
     computerSelection = computerChoice;
     console.log(`Computer selection was ${computerSelection}`);
 
     /* Declares winning string, tie and loosing string */
     let winString = `You Win! ${playerSelection} beats ${computerSelection}`;
     let tieString = "It's a tie!"
-    let loseString = `You Lose ${computerSelection} beats ${playerSelection}`;
+    let loseString = `You Lose... ${computerSelection} beats ${playerSelection}`;
 
     /* Checks if input equals Rock Paper or Scisors and returns 'not a valid option' if not */
     if (playerSelection != 'Rock' && playerSelection != 'Paper' && playerSelection != 'Scissors' ) {
@@ -60,19 +65,26 @@ function oneRound(playerSelection, computerSelection) {
         if (playerSelection == computerSelection) {
             return tieString;
         } else if ( (playerSelection == 'Rock' && computerSelection == 'Paper') || (playerSelection == 'Paper' && computerSelection == 'Scissors') || (playerSelection == 'Scissors' && computerSelection == 'Rock')){
+            ++computerScore;
             return loseString;    
         } else if ( (playerSelection == 'Rock' && computerSelection == 'Scissors') || (playerSelection == 'Paper' && computerSelection == 'Rock') || (playerSelection == 'Scissors')) {
+            ++playerScore;
             return winString;
         }
     }
 }
 
+/* Declares score variables of type number with initial value of 0 */
+  let computerScore = 0;
+  let playerScore = 0;
+
 /* Create function named 'game()' that 
 runs 'oneRound' 5 times to make a 5 round game */
 function game(rounds){
     for (var i = 0; i < rounds; i++){
-        console.log(i);
         console.log(oneRound());
+        console.log(`Computer Score: ${computerScore}`);
+        console.log(`Player Score: ${playerScore}`);
     }
 }
 
