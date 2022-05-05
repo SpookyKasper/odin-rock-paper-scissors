b54eca054824edb9defc0e75c7def41c7ed9a898
Kasper <kasperbak89@pm.me>
2022-05-06 00:26:56 +0200

    Clean unnecessary code

--- rps.js
+++ rps.js
@@ -1,10 +1,9 @@
 
 /*---------------------------------- Create a Rock Paper Scissors game where the player plays against the computer --------------------------------*/
 
-/* Write a function that plays a single round of Rock Paper Scissors, 
-take two parameters' playerSelection' and 'computerSelection'
-compare the parameters and return a string 
-
+/* This function plays a single round of Rock Paper Scissors, it 
+takes two parameters 'playerSelection' and 'computerSelection',
+compares them and return an adapted string. 
 
 /* Randomly returns an integer between 0 and passed argument */
 function getRandomInt(max) {
@@ -14,76 +13,44 @@ function getRandomInt(max) {
 /* Contains the 3 computer options for the game */
 let rpsArray = ['Rock', 'Paper', 'Scissors'];
 
-/* Uses 'getRandomInt' to randomly returns either 'Rock', 'Paper' or 'Scissors' from the 'rpsArray' array */
+/* Uses 'getRandomInt' to randomly 
+return either 'Rock', 'Paper' or 'Scissors' from 'rpsArray' */
 function computerPlay() {
     return rpsArray[getRandomInt(3)];
 }
-
-/*  Checks if empty or capitalize passed string  */
-function cleanString(someString) {
-    if (someString === '' ) {
-        return 'nothing';
-    } else {
-        return someString[0].toUpperCase() + someString.slice(1).toLowerCase();
-    }
-}
-
-/* Declares variable named 'playerChoice' of type string that
-stores capitalized user input of the */
-// let playerChoice = cleanString(prompt('Rock Paper or Scissors ?'));
-
 
 /* One Round: Compare the two selections and return adapted string */
 function oneRound(playerSelection, computerSelection) {
 
     /* Assigns to computerSelection the result of computerPlay function */
     computerSelection = computerPlay();
+    
+    /* alerts what the user and computer selection are */
+    alert(`You picked ${playerSelection} the computer picked ${computerSelection}`)
 
-    /* Declares winning string, tie and loosing string */
+    /* Declares three variables of type string with initial value of respectively winning string, tie and loosing string*/
     let winString = `You won the round! ${playerSelection} beats ${computerSelection}`;
     let tieString = "It's a tie!"
     let loseString = `You lost this round bro... ${computerSelection} beats ${playerSelection}`;
 
-    /* Checks if input equals Rock Paper or Scisors and returns 'not a valid option' if not */
-    if (playerSelection != 'Rock' && playerSelection != 'Paper' && playerSelection != 'Scissors' ) {
-        console.log('invalid input');
-        alert(`Arf! you typed ${playerSelection} and it's kind of different from `
-            + `Rock, Paper or Scissors and my (stupid) computer program doesn't like that... `
-            + `please type Rock, Paper or Scissors`)
-        return undefined;
-
+    /* Compares inputs and returns a string depending on who wins */ 
+    if (playerSelection == computerSelection) {
+        alert(`${tieString} 
+        Computer Score: ${computerScore} 
+        Player Score: ${playerScore}`);
+        return 'tie';
+    } else if ( (playerSelection == 'Rock' && computerSelection == 'Paper') || (playerSelection == 'Paper' && computerSelection == 'Scissors') || (playerSelection == 'Scissors' && computerSelection == 'Rock')){
+        ++computerScore;
+        alert(`${loseString}
+        Computer Score: ${computerScore} 
+        Player Score: ${playerScore}`);
+        return 'lose';
     } else {
-        /* alerts what the user and computer selection are */
-        alert(`You picked ${playerSelection} the computer picked ${computerSelection}`)
-        /* Compares computerSelection and playerSelection and 
-        returns a string depending on the result of the comparaison */ 
-        if (playerSelection == computerSelection) {
-            console.log(tieString);
-            console.log(`Computer Score: ${computerScore}`);
-            console.log(`Player Score: ${playerScore}`);
-            alert(`${tieString} 
-            Computer Score: ${computerScore} 
-            Player Score: ${playerScore}`);
-            return 'tie';
-        } else if ( (playerSelection == 'Rock' && computerSelection == 'Paper') || (playerSelection == 'Paper' && computerSelection == 'Scissors') || (playerSelection == 'Scissors' && computerSelection == 'Rock')){
-            ++computerScore;
-            console.log(loseString);
-            console.log(`Computer Score: ${computerScore}`);
-            console.log(`Player Score: ${playerScore}`);
-            alert(`${loseString}
-            Computer Score: ${computerScore} 
-            Player Score: ${playerScore}`);
-            return 'lose';
-        } else if ( (playerSelection == 'Rock' && computerSelection == 'Scissors') || (playerSelection == 'Paper' && computerSelection == 'Rock') || (playerSelection == 'Scissors')) {
-            ++playerScore;
-            console.log(winString);
-            console.log(`Computer Score: ${computerScore}`);
-            console.log(`Player Score: ${playerScore}`);
-            alert(`${winString}
-            Computer Score: ${computerScore} 
-            Player Score: ${playerScore}`);
-            return 'win';
-        }
+        ++playerScore;
+        alert(`${winString}
+        Computer Score: ${computerScore} 
+        Player Score: ${playerScore}`);
+        return 'win';
     }
 }
 
