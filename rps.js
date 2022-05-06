57015b995992ca31d3065885cb2a2aac6f1bf26d
Kasper <kasperbak89@pm.me>
2022-05-06 03:13:05 +0200

    Add logic, end of game and restart
    Now when the player or the computer reach 5 points
    The winner is announced on the screen and the user
    needs to press the restart button to continue playing

--- rps.js
+++ rps.js
@@ -2,7 +2,7 @@
 /*---------------------------------- Create a Rock Paper Scissors game where the player plays against the computer --------------------------------*/
 
 /* This function plays a single round of Rock Paper Scissors, it 
-takes two parameters 'playerSelection' and 'computerSelection',
+takes two parameters 'playerPick' and 'botPick',
 compares them and return an adapted string. 
 
 /* Randomly returns an integer between 0 and passed argument */
@@ -19,48 +19,90 @@ function computerPlay() {
     return rpsArray[getRandomInt(3)];
 }
 
-let result = '';
+// Declare a variable named gameIsOver of type boolean with initial value of false
+let gameIsOver = false;
+
+
+// Declares a function named restart to reset the game
+function restart() {
+    gameIsOver = false;
+    resultBox.textContent = `Let's go! choose your fighter!`;
+    computerScore = 0;
+    playerScore = 0;
+}
 
 
 /* One Round: Compare the two selections and return adapted string */
-function oneRound(playerSelection, computerSelection) {
-    
-    /* Assigns to computerSelection the result of computerPlay function */
-    computerSelection = computerPlay();
+function oneRound(playerPick, botPick) {
+   
+    /* Assigns to botPick the result of computerPlay function */
+    botPick = computerPlay();
     
     /* alerts what the user and computer selection are */
-    resultBox.textContent = `You picked ${playerSelection} the computer picked ${computerSelection}`;
+    resultBox.textContent = `You picked ${playerPick} the computer picked ${botPick}`;
     
-    /* Declares three variables of type string with initial value of respectively winning string, tie and loosing string*/
-    let winString = `You won the round! ${playerSelection} beats ${computerSelection}`;
+    /* Declares variables of type string with initial value of respectively winning string, tie and loosing string*/
+    let winString = `You won the round! ${playerPick} beats ${botPick}`;
     let tieString = "It's a tie!"
-    let loseString = `You lost this round bro... ${computerSelection} beats ${playerSelection}`;
-    
+    let loseString = `You lost this round bro... ${botPick} beats ${playerPick}`;
+    let winGameString = `Congratulations! The game is over and you Won! Wouhouuouo!!!`;
+    let loseGameString = `Damn bro you where so close! try again!`;
+
+    let tieFullQuote = `
+
+    ${tieString} 
+    Computer Score: ${computerScore} 
+    Player Score: ${playerScore}`;
+
+
+    if(gameIsOver){
+        resultBox.textContent = `Game is over! if you want to play another time click the restart button!`;
+        return;
+    }
+
     /* Compares inputs and returns a string depending on who wins */ 
-    if (playerSelection == computerSelection) {
-        resultBox.textContent += `
-        ${tieString} 
-        Computer Score: ${computerScore} 
-        Player Score: ${playerScore}`;
+    if (playerPick == botPick) {
+        resultBox.textContent += tieFullQuote;
         return 'tie';
-    } else if ( (playerSelection == 'Rock' && computerSelection == 'Paper') || (playerSelection == 'Paper' && computerSelection == 'Scissors') || (playerSelection == 'Scissors' && computerSelection == 'Rock')){
+    } else if ( (playerPick == 'Rock' && botPick == 'Paper') || (playerPick == 'Paper' && botPick == 'Scissors') || (playerPick == 'Scissors' && botPick == 'Rock')){
         ++computerScore;
+        if (computerScore >= 5){
+            resultBox.textContent += `
+
+            ${loseGameString}
+            Computer Score: ${computerScore} 
+            Player Score: ${playerScore}`;
+            gameIsOver = true;
+            return;
+        }
         resultBox.textContent += `
+
         ${loseString}
         Computer Score: ${computerScore} 
         Player Score: ${playerScore}`;
-        return 'lose';
+        return;
     } else {
         ++playerScore;
+        if(playerScore >= 5){
+            resultBox.textContent += `
+            
+            ${winGameString}
+            Computer Score: ${computerScore} 
+            Player Score: ${playerScore}`;
+            gameIsOver = true;
+            return;
+        }
         resultBox.textContent += `
+
         ${winString}
         Computer Score: ${computerScore} 
         Player Score: ${playerScore}`;
-        return 'win';
+        return;
 
-    }
-    
+    }    
 }
+
+
 
 
 /* Declares score variables of type number with initial value of 0 */
@@ -77,8 +119,6 @@ function game(rounds){
 
         /* Declares winning or losing the game variables of type string 
         with declared inital value */
-        let winGameString = `Congratulations! The game is over and you Won! Wouhouuouo!!!`;
-        let loseGameString = `Damn bro you where so close! try again!`;
 
         /* Checks if roundResult equals tie or invalid and if true don't count round */
         if (roundResult == 'tie' || roundResult == undefined) {
